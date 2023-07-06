75d12f0aaffe964f4759a8b3948bd78f67cf4fdf
charlotteg1 <charlotte.gould3@gmail.com>
2023-07-06 15:39:15 +0100

    add first comparison

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class WeatherCheck {
+public class WeatherCheck {
+    public static void main(String[] args) {
+        int temperature = 23;
+        boolean currentlyRaining = true;
+        if((temperature>=15) && currentlyRaining==false){
+            System.out.println("no coat needed");
+            
+        }
+
+    }
 }
