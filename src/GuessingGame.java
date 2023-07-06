e615131b125ef41fe0709fa917238b8721245dff
charlotteg1 <charlotte.gould3@gmail.com>
2023-07-06 16:22:23 +0100

    Adding in comments

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,12 +1,12 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        int secretNumber = 47;
+        int secretNumber = 47; // Number aiming to guess
         int guess = 36;
         if( guess == secretNumber){
             System.out.println("Correct");
-        }else if(guess > secretNumber){
+        }else if(guess > secretNumber){ //test if greater than
             System.out.println("Too High");
-        }else{
+        }else{ //this result is because it is less than
             System.out.println("Too Low");
         }
     }
--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -1,12 +1,14 @@
 public class WeatherCheck {
     public static void main(String[] args) {
-        int temperature = 17;
+        int temperature = 13;
         boolean currentlyRaining = false;
-        if((temperature>=13) && currentlyRaining==false){
+        // below will print out whether a coat is needed or not
+        if((temperature>=13) && currentlyRaining==false){ //Test to see if it's hot
             System.out.println("No coat needed!");
-        }else if(currentlyRaining && temperature>=13){
+        }else if(currentlyRaining && temperature>=13){ //Hot but raining
             System.out.println("Bring umbrella or wear rain mac");
-        }else if (temperature<13 || currentlyRaining){ // here the || currently raining would be redundant
+        }else if (temperature<13 || currentlyRaining){ // See if cold.
+            // Here the || currently raining would be redundant. Could use
             System.out.println("Wear a coat");
         }
     }
